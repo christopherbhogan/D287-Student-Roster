a50a8ec3c05f009bd877f3c33c79c23df555f941
Christopher Hogan <christopherbhogan1@gmail.com>
2026-07-25 06:52:53 -0500

    initial commit - task A

---
+++ src/Student_Main.java
@@ -0,0 +1,9 @@
+public class Student_Main {
+    public static void main(String[] args) {
+        String[] students = {"1,John,Smith,John1989@gmail.com,20,88,79,59",
+        "2,Suzan,Erickson,Erickson_1990@gmail.com,19,91,72,85",
+        "3,Jack,Napoli,The_lawyer99yahoo.com,19,85,84,87",
+        "4,Erin,Black,Erin.black@comcast.net,22,91,98,82",
+        "5,firstName,lastName,myemail@email.com,34,85,89,93"}; // replace with name
+    }
+}
