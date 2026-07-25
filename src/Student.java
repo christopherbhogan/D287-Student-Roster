e04e9e5b0805c786f4cacefa97ff0080ed6c2d25
Christopher Hogan <christopherbhogan1@gmail.com>
2026-07-25 07:46:24 -0500

    Add Student class - part B1 complete

---
+++ src/Student.java
@@ -0,0 +1,10 @@
+public class Student {
+    private String studentID;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private int age;
+    private int[] grade;
+
+
+}
