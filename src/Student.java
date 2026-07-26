8b752a2d797a721a43dc27bd454f08b9df142496
Christopher Hogan <christopherbhogan1@gmail.com>
2026-07-26 11:59:15 -0500

    Add getters, setters, and constructor to Student class - task B complete

--- src/Student.java
+++ src/Student.java
@@ -1,10 +1,76 @@
+import java.util.Arrays; // to convert array of grades to readable text
+
 public class Student {
     private String studentID;
     private String firstName;
     private String lastName;
     private String email;
     private int age;
-    private int[] grade;
+    private int[] grades;
 
-    // part B1 complete
+    public Student(String studentID, String firstName, String lastName, String email, int age, int[] grades) {
+        this.studentID = studentID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.age = age;
+        this.grades = grades;
+    }
+
+    public String getStudentID() {
+        return studentID;
+    }
+
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int[] getGrades() {
+        return grades; // currently the grades array may be accessed by means other than setGrades further research needed
+    }
+
+    public void setGrades(int[] grades) {
+        this.grades = grades;
+    }
+
+    public void print() {
+        System.out.println("Student ID: " + getStudentID());
+        System.out.println("First Name: " + getFirstName());
+        System.out.println("Last Name: " + getLastName());
+        System.out.println("E-mail: " + getEmail());
+        System.out.println("Age: " + getAge());
+        System.out.println("Grades: " + Arrays.toString(getGrades()));
+    }
+
 }
--- src/Student_Main.java
+++ src/Student_Main.java
@@ -4,6 +4,6 @@ public class Student_Main {
         "2,Suzan,Erickson,Erickson_1990@gmail.com,19,91,72,85",
         "3,Jack,Napoli,The_lawyer99yahoo.com,19,85,84,87",
         "4,Erin,Black,Erin.black@comcast.net,22,91,98,82",
-        "5,firstName,lastName,myemail@email.com,34,85,89,93"}; // replace with name
+        "5,firstName,lastName,myemail@email.com,34,85,89,93"}; // replace with personal details
     }
 }
